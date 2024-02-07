374c52284ea6fefb00cbb06ce67f34e5c2c7cf82
Samiul Monir <samiulmonir@gmail.com>
2024-02-07 08:32:47 -0500

    problem set 2

---
+++ problems/problem-2.js
@@ -0,0 +1,13 @@
+/**
+ 1. How do you concat two arrays? Write a function to concat two arrays.
+    Example: arr1 = [1,2,3,4,5], arr2 = [6,7,8,9,10]
+ 2. Write a function to print first 25 fibonacci numbers. 
+    https://www.mathsisfun.com/numbers/fibonacci-sequence.html
+ 3. Write a function to print all primes for given value n.
+    Primes numbers are only divisible by 1 and itself.
+ 4. Write a function to count how many primes numbers are present for a given value n.
+ 5. Write a function to determine if the given word is palindrome.
+    Palindrome is a word, verse, or sentence (such as "Retter", "racecar") 
+    or a number (such as 1881) that reads the same backward or forward
+ */
+
