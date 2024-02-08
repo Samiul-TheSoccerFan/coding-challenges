29474ed055bae5621681290fe92eba412b127a32
Samiul Monir <samiulmonir@gmail.com>
2024-02-07 21:19:54 -0500

    problem set 3

---
+++ problems/problem-3.js
@@ -0,0 +1,36 @@
+/**
+ 1. Given an array of integers nums, return the number of good pairs. A pair (i, j) is called good if nums[i] == nums[j] and i < j.
+    Input: nums = [1,2,3,1,1,3]
+    Output: 4
+    Explanation: There are 4 good pairs (0,3), (0,4), (3,4), (2,5) 0-indexed.
+
+    Input: nums = [1,1,1,1]
+    Output: 6
+    Explanation: Each pair in the array are good.
+
+    Input: nums = [1,2,3]
+    Output: 0
+
+ 2. Given a number, write a function to calculate the sum of the digits.
+    arr = 123
+    output = 6; 1 + 2 + 3 = 6
+ 3. Given a non-empty array of integers nums, every element appears twice except for one. Find that single one. 
+    Assume only one will appear once.
+
+    nums = [2,2,1]
+    output: 1
+
+    nums = [4,1,2,1,2]
+    Output: 4
+
+    nums = [1]
+    Output: 1
+ 4. Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must be unique and you may return the result in any order.
+
+    Input: nums1 = [1,2,2,1], nums2 = [2,2]
+    Output: [2]
+
+    Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+    Output: [9,4]
+    Explanation: [4,9] is also accepted.
+ */
