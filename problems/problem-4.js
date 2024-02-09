e21cb51fc780cc0545a6b70bd0a4e438582effad
Samiul Monir <samiulmonir@gmail.com>
2024-02-08 20:04:27 -0500

    Added problem Set 4

---
+++ problems/problem-4.js
@@ -0,0 +1,11 @@
+/**
+Let's build a guessing game preferably in React. Generate a random number and take input from user using an input field and show a label below:
+
+- Guessed right; If the input and generated random matches.
+- Too low; If the guessed number is lower than the generated random number.
+- Too high; If the guessed number is higher than the generated random number.
+
+Keep the count of the number of guesses.
+
+Note: if you want to skip React and want to do with only JS, that is also good too.
+*/
